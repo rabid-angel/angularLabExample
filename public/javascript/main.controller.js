06e8ebe9c61ddec4d654fcd0ad7b47a45715501a
BizarreKitten <senxx041@morris.umn.edu>
2015-01-29 17:53:07 -0600

    set up gpa calculator ui

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -48,3 +48,37 @@ var mainApp = angular.module("mainApp", []);
            return $scope.data.length;
        };
     });
+
+//==================== GPA CONTROLLER ==================================
+mainApp.controller('gpaCtrl', function($scope){
+    console.log("controller loaded!");
+
+    $scope.classField = "";
+    $scope.creditsField = "";
+    $scope.gradeField = "";
+
+    // Normally, data like this would be stored in a database, and this controller would issue an http:get request for it.
+    $scope.data = [
+    ];
+
+    $scope.addData = function(){
+        if($scope.classField.length >= 1 && $scope.creditsField.length >= 1 && $scope.gradeField.length  == 1) {
+            $scope.data.push({class: $scope.classField, credits: $scope.creditsField, grade: $scope.gradeField});
+            $scope.classField = "";
+            $scope.creditsField = "";
+            $scope.gradeField = "";
+        }
+    };
+
+    $scope.removeData = function(index){
+        $scope.data.splice(index, 1);
+    };
+
+    $scope.cat = function(str1, str2){
+        return str1 + str2;
+    };
+
+    $scope.itemsInList = function(){
+        return $scope.data.length;
+    };
+});
