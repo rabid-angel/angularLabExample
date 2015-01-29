90ebe75d1af8db3e0b3676d3ee140b43c85b7a7b
rabid-angel <hagen715@morris.umn.edu>
2015-01-29 13:47:54 -0600

    Created a GPA page.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -8,7 +8,8 @@ var mainApp = angular.module("mainApp", []);
             {text: "Home", link: '/'},
             {text: "About", link: '/about'},
             {text: "Kittens", link: '/kittens'},
-            {text: "404 Page", link: '/wefwrtbertbeb'}
+            {text: "404 Page", link: '/wefwrtbertbeb'},
+            {text: "Pet GPA", link: '/GPA'}
         ];
     });
 
